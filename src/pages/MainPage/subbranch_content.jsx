19f808675f65f2abacce49df91ba632e13534114
turalbabayev <turalbabayev@outlook.com.tr>
2025-04-08 16:46:48 +0300

    Toplu Soru İndirme Özelliği Eklendi

--- src/pages/MainPage/subbranch_content.jsx
+++ src/pages/MainPage/subbranch_content.jsx
@@ -147,9 +147,35 @@ function SubbranchContent() {
                                     {altDal.sorular ? (
                                         <div className="space-y-4">
                                             <div className="flex justify-between items-center mb-4">
-                                                <span className="text-gray-600 dark:text-gray-400">
-                                                    {Object.keys(altDal.sorular).length} Soru
-                                                </span>
+                                                <div className="flex items-center space-x-4">
+                                                    <span className="text-gray-600 dark:text-gray-400">
+                                                        {Object.keys(altDal.sorular).length} Soru
+                                                    </span>
+                                                    <button
+                                                        onClick={() => {
+                                                            setSelectedAltDal(key);
+                                                            setIsBulkDeleteOpen(true);
+                                                        }}
+                                                        className="flex items-center space-x-2 bg-red-600 hover:bg-red-700 text-white px-3 py-1.5 rounded-lg shadow-sm transition-all"
+                                                    >
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                                                        </svg>
+                                                        <span>Toplu Sil</span>
+                                                    </button>
+                                                    <button
+                                                        onClick={() => {
+                                                            setSelectedAltDal(key);
+                                                            setIsBulkDownloadOpen(true);
+                                                        }}
+                                                        className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-lg shadow-sm transition-all"
+                                                    >
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                                                        </svg>
+                                                        <span>Toplu İndir</span>
+                                                    </button>
+                                                </div>
                                             </div>
                                             {sortedQuestions(altDal.sorular).map(([soruKey, soru]) => (
                                                 <div key={soruKey} className="bg-gray-50 dark:bg-gray-700 rounded-lg p-4">
