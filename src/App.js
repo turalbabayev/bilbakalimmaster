2d3d9b5f8047bf4287dafc2cc8382030d3c319cc
turalbabayev <turalbabayev@outlook.com.tr>
2025-04-10 11:31:09 +0300

    App.js dosyasındaki import yolları düzeltildi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/home";
-import Question from "./pages/question";
+import Home from "./pages/MainPage/home_page";
+import Question from "./pages/MainPage/questions_page";
 import QuestionContent from "./pages/MainPage/question_content_page";
-import Login from "./pages/auth/login";
-import PrivateRoute from "./components/PrivateRoute";
-import AnnouncementsPage from "./pages/announcements/announcements_page";
+import Login from "./pages/login_page";
+import AnnouncementPage from "./pages/MainPage/announcement_page";
 import UserList from "./pages/Users/UserList";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Toaster } from 'react-hot-toast';
+import ProtectedRouter from './routes/ProtectedRoute';
 
 function App() {
   return (
@@ -18,41 +18,41 @@ function App() {
         <Route
           path="/home"
           element={
-            <PrivateRoute>
+            <ProtectedRouter>
               <Home />
-            </PrivateRoute>
+            </ProtectedRouter>
           }
         />
         <Route
           path="/question"
           element={
-            <PrivateRoute>
+            <ProtectedRouter>
               <Question />
-            </PrivateRoute>
+            </ProtectedRouter>
           }
         />
         <Route
           path="/question/:id"
           element={
-            <PrivateRoute>
+            <ProtectedRouter>
               <QuestionContent />
-            </PrivateRoute>
+            </ProtectedRouter>
           }
         />
         <Route
           path="/announcements"
           element={
-            <PrivateRoute>
-              <AnnouncementsPage />
-            </PrivateRoute>
+            <ProtectedRouter>
+              <AnnouncementPage />
+            </ProtectedRouter>
           }
         />
         <Route
           path="/users"
           element={
-            <PrivateRoute>
+            <ProtectedRouter>
               <UserList />
-            </PrivateRoute>
+            </ProtectedRouter>
           }
         />
         <Route path="/" element={<Login />} />
