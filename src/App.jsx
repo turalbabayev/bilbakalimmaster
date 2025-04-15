19bda5640cffc820e710859281b2c632470c7774
turalbabayev <turalbabayev@outlook.com.tr>
2025-04-15 23:09:18 +0300

    fix: App.jsx'e DenemeSinavlariPage import'u eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,32 @@
 import ImportQuestionsFromDocx from "./components/ImportQuestionsFromDocx";
-import ImportQuestionsFromJSON from "./components/ImportQuestionsFromJSON"; 
+import ImportQuestionsFromJSON from "./components/ImportQuestionsFromJSON";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import MainPage from "./pages/MainPage";
+import DenemeSinavlariPage from "./pages/DenemeSinavlari/DenemeSinavlariPage";
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Toaster
+                    position="top-center"
+                    reverseOrder={false}
+                    toastOptions={{
+                        duration: 3000,
+                        style: {
+                            background: '#333',
+                            color: '#fff',
+                        },
+                    }}
+                />
+                <Routes>
+                    <Route path="/*" element={<MainPage />} />
+                    <Route path="/deneme-sinavlari" element={<DenemeSinavlariPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App; 
