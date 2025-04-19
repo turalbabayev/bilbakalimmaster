18dcafbd1a6bc744aac04a4e43fb45eef571cd75
turalbabayev <turalbabayev@outlook.com.tr>
2025-04-19 18:23:10 +0300

    App.js'de bildirimler route'u düzeltildi ve ProtectedRouter ile sarmalandı

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginPage from './pages/login_page';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LoginPage from "./pages/login_page";
 import HomePage from './pages/MainPage/home_page';
 import QuestionsPage from './pages/MainPage/questions_page';
 import QuestionContent from './pages/MainPage/question_content_page';
@@ -8,28 +9,29 @@ import SubbranchContent from './pages/MainPage/subbranch_content';
 import AnnouncementPage from './pages/MainPage/announcement_page';
 import GamesPage from './pages/MainPage/games_page';
 import NotesPage from './pages/MainPage/notes_page';
+import NotificationsPage from './pages/MainPage/notifications_page';
 import ProtectedRouter from './routes/ProtectedRoute';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <AuthProvider>
-       <Router>
-          <Routes>
-            <Route path='/' element={<LoginPage />} />
-            <Route path='/home' element={<ProtectedRouter><HomePage /></ProtectedRouter>} />
-            <Route path='/question' element={<ProtectedRouter><QuestionsPage /></ProtectedRouter>} />
-            <Route path='/question/:id' element={<ProtectedRouter><QuestionContent /></ProtectedRouter>}/>
-            <Route path='/question/:konuId/:altKonuId' element={<ProtectedRouter><SubbranchContent/></ProtectedRouter>} />
-            <Route path='/announcements' element={<ProtectedRouter><AnnouncementPage /></ProtectedRouter>} />
-            <Route path='/games' element={<ProtectedRouter><GamesPage /></ProtectedRouter>} />
-            <Route path='/notes' element={<ProtectedRouter><NotesPage /></ProtectedRouter>} />
-            <Route path='/notifications' element={<ProtectedRouter><NotificationsPage /></ProtectedRouter>} />
-          </Routes>
-        </Router>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <Toaster position="top-right" />
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path='/home' element={<ProtectedRouter><HomePage /></ProtectedRouter>} />
+                    <Route path='/question' element={<ProtectedRouter><QuestionsPage /></ProtectedRouter>} />
+                    <Route path='/question/:id' element={<ProtectedRouter><QuestionContent /></ProtectedRouter>}/>
+                    <Route path='/question/:konuId/:altKonuId' element={<ProtectedRouter><SubbranchContent/></ProtectedRouter>} />
+                    <Route path='/announcements' element={<ProtectedRouter><AnnouncementPage /></ProtectedRouter>} />
+                    <Route path='/games' element={<ProtectedRouter><GamesPage /></ProtectedRouter>} />
+                    <Route path='/notes' element={<ProtectedRouter><NotesPage /></ProtectedRouter>} />
+                    <Route path='/notifications' element={<ProtectedRouter><NotificationsPage /></ProtectedRouter>} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
