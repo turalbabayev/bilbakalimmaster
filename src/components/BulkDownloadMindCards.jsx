294b9ddb398dba63ccf893889b2201612404c2e0
turalbabayev <turalbabayev@outlook.com.tr>
2025-05-05 19:59:28 +0300

    Firebase import yolu düzeltildi

--- src/components/BulkDownloadMindCards.jsx
+++ src/components/BulkDownloadMindCards.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { db } from "../../firebase";
+import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { Document, Packer, Paragraph, TextRun, HeadingLevel } from "docx";
 import { saveAs } from "file-saver";
